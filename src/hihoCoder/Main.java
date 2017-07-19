9c61c116c025e0315bb59a3bfb5d5bb38cfa39c5
mindawei <3092527496.qq.com>
2017-07-19 14:13:53 +0800

    save

---
+++ src/hihoCoder/Main.java
@@ -0,0 +1,21 @@
+package hihoCoder;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String str, substr;
+//		int num = in.nextInt();
+//		for(int i=0;i<num;++i){
+//			substr = in.next();
+//			str = in.next();
+//			System.out.println(findByKmp(str, substr));
+//		}
+		in.close();
+	}
+
+
+
+}
