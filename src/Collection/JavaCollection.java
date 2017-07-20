eb7320b26e6d202447dea6775d6a712137c3078b
mindawei <3092527496.qq.com>
2017-07-20 16:06:30 +0800

    save

---
+++ src/Collection/JavaCollection.java
@@ -0,0 +1,36 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class JavaCollection {
+	
+	public static void main(String[] args) {
+		
+		HashMap hashMap = new HashMap<>();
+
+		LinkedHashMap linkedHashMap = new LinkedHashMap<>();
+		
+		TreeMap treeMap = new TreeMap<>();
+		
+		ConcurrentHashMap concurrentHashMap =  new ConcurrentHashMap<>();
+		
+		TreeSet treeSet = new TreeSet<>();
+		
+		HashSet hashSet = new HashSet<>();
+		
+		LinkedList linkedList = new LinkedList<>();
+		
+		ArrayList arrayList = new ArrayList<>();
+		
+		
+		
+	}
+
+}
