756407f0d7340627560a941c0433118c5b7eafda
mindawei <mindw>
2017-08-09 18:57:47 +0800

    save

---
+++ src/StaticTest.java
@@ -0,0 +1,8 @@
+
+public class StaticTest {
+	
+	    public static void main(String[] args) {
+	        System.out.println(SubClass.value);
+
+	    }
+}
---
+++ src/SubClass.java
@@ -0,0 +1,6 @@
+
+public class SubClass extends SuperClass {
+	    static {
+	        System.out.println("SubClass init!");
+	    }
+	}
