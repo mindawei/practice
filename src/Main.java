6135303889e77c11b43d90c178c49a3080e62d2e
mindawei <mindw>
2017-08-14 23:05:23 +0800

    save

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for(int i=0;i<n;++i){
-			arr[i] = sc.nextInt();
-		}
-		System.out.println(solve(arr));
-	}
-
-	private static String solve(int[] arr) {
-	
-		return null;
+		System.out.println(Math.pow(2+Math.sqrt(3),80)%10);
 	}
 }
+	
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int[] arr = new int[n];
+//		for(int i=0;i<n;++i){
+//			arr[i] = sc.nextInt();
+//		}
+//		System.out.println(solve(arr));
+//	}
+//
+//	private static String solve(int[] arr) {
+//	
+//		return null;
+//	}
+//}
