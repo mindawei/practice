1ca3a86536afe6498084008213ab7fcf0e42d3c9
mindw <mindw@192.168.0.100>
2017-09-14 21:21:43 +0800

    save

--- src/company/Main.java
+++ src/company/Main.java
@@ -1,12 +1,51 @@
 package company;
 
-
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-	
+		int n = sc.nextInt();
+		long[] v = new long[n];
+		
+		int[] mods = new int[7];
+		for(int i=0;i<n;++i) {
+			int num  = sc.nextInt();
+			v[i] = num;
+			mods[num%7]++;
+		}
+		
+		int ans = 0;
+		for(int i=0;i<n;++i) {
+			
+			long num = v[i];
+			int ys = (int)(num % 7);
+			
+			if(ys==0) {
+				ans+=n-1;
+			}else {
+				
+				long base = 1;
+				long left = num;
+				while(left>0) {
+					left/=10;
+					base *=10;
+				}
+				
+				for(int j=1;j<7;++j) {
+					if((base*j+num) % 7==0) {
+					     if(ys==j) {
+					    	 ans += mods[j]-1;
+					     }else {
+					    	 ans += mods[j];
+					     }
+					}
+				}
+			}
+			
+		}
+		
+		System.out.println(ans);
 	}
 }
--- src/company/Main2.java
+++ src/company/Main2.java
@@ -1,6 +1,8 @@
 package company;
 
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main2 {
@@ -8,5 +10,25 @@ public class Main2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 	
+		int n = Integer.parseInt(sc.nextLine());
+		Map<String,Integer> map = new HashMap<>();
+		for(int i=0;i<n;++i) {
+			String line = sc.nextLine();
+			if(line.charAt(0)=='?') {
+				String key = line.substring(2);
+				System.out.println(map.get(key));
+			}else {
+				String[] parts = line.split(" = ");
+				String key = parts[0];
+				String value = parts[1];
+				
+				if(Character.isDigit(value.charAt(0))) {
+					map.put(key, Integer.parseInt(value));
+				}else {
+					map.put(key, map.get(value));
+				}
+			}
+		}
 	}
+	
 }
